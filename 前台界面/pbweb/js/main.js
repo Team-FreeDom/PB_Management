53907e9bf3062049e0eade1a62d4e7005c0ba053
zaoying1027 <1747266224@qq.com>
2016-11-10 10:54:58 +0800

    界面

--- 前台界面/pbweb/js/main.js
+++ 前台界面/pbweb/js/main.js
@@ -1,3 +1,82 @@
 	$("li.menuItem.nav-parent").click(function(){
 	  $(this).toggleClass("opened nav-expanded");
 	})
+	
+	
+ $(document).ready(function() {
+              $('#table1').dataTable(
+			  {
+				  "bSort": false,
+				  "bFilter": false,
+				  "bLengthChange": false,
+				  "scrollY": 75,
+				  "aLengthMenu":[2,4,5,8], //动态指定分页后每页显示的记录数。
+					"lengthChange":true, //是否启用改变每页显示多少条数据的控件
+					"iDisplayLength" : 2,  //默认每页显示多少条记录
+                    "language": {
+                        "lengthMenu": "每页 _MENU_ 条记录",
+                        "zeroRecords": "没有找到记录",
+                        "info": "第 _PAGE_ 页 ( 总共 _PAGES_ 页 )",
+                        "infoEmpty": "无记录",
+                        "infoFiltered": "(从 _MAX_ 条记录过滤)",
+						"sSearch": "模糊查询：",
+						"oPaginate": {
+						   "sFirst": "首页",
+						   "sPrevious": " 上一页 ",
+						   "sNext": " 下一页 ",
+						   "sLast": " 尾页 "
+					   }
+                    }
+			  });	  
+            } );
+			
+ $(document).ready(function(){
+				$('#table2').DataTable(
+				{
+					"aLengthMenu":[5,10,15,20], //动态指定分页后每页显示的记录数。
+					"lengthChange":true, //是否启用改变每页显示多少条数据的控件
+					"iDisplayLength" : 5,  //默认每页显示多少条记录
+				  "bSort": false,
+				  "bFilter": false,
+				  "bLengthChange": false,
+				  "language": {
+                        "lengthMenu": "每页 _MENU_ 条记录",
+                        "zeroRecords": "没有找到记录",
+                        "info": "第 _PAGE_ 页 ( 总共 _PAGES_ 页 )",
+                        "infoEmpty": "无记录",
+                        "infoFiltered": "(从 _MAX_ 条记录过滤)",
+						"sSearch": "模糊查询：",
+						"oPaginate": {
+						   "sFirst": "首页",
+						   "sPrevious": " 上一页 ",
+						   "sNext": " 下一页 ",
+						   "sLast": " 尾页 "
+					   }
+                    }
+					
+				
+				});
+			});
+			
+			$(document).ready(function(){
+				$('#table3').DataTable(
+				{
+					"bLengthChange": false,
+					 "language": {
+                        "lengthMenu": "每页 _MENU_ 条记录",
+                        "zeroRecords": "没有找到记录",
+                        "info": "第 _PAGE_ 页 ( 总共 _PAGES_ 页 )",
+                        "infoEmpty": "无记录",
+                        "infoFiltered": "(从 _MAX_ 条记录过滤)",
+						"sSearch": "模糊查询：",
+						"oPaginate": {
+						   "sFirst": "首页",
+						   "sPrevious": " 上一页 ",
+						   "sNext": " 下一页 ",
+						   "sLast": " 尾页 "
+					   }
+                    }
+					}
+				
+				);
+			});
