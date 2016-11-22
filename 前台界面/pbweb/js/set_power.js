8b0a66a5c4ecb9bfc8f25592a3bf69d410e7de77
hnspoc <18349995@qq.com>
2016-11-22 20:43:55 +0800

    土地租赁js提交数据修改

--- 前台界面/pbweb/js/set_power.js
+++ 前台界面/pbweb/js/set_power.js
@@ -1,7 +1,31 @@
 // JavaScript Document
 $(function () {
-	var serializedData=[{id:1,name:'身份名',pow:''}];
 
+	
+	var tudi=new function () {
+		this.sfData=[{id:1,name:'系统管理员'},{id:2,name:'普通教师'},{id:3,name:'租赁业务专员'},{id:4,name:'实习业务专员'}
+		];
+		this.powData=[{id:11,pname:'土地租赁审批',v:true},{id:12,pname:'实习审批',v:true},{id:13,pname:'实习基地审批',v:true},{id:4,pname:'基地维护审批',v:true},{id:21,pname:'土地租赁维护',v:true}
+		];
+		
+		this.loaddata= function(){
+			str='<a href="#" class="list-group-item active">管理身份列表</a>';
+			 _.map(this.sfData,function(em){
+				 str=str+'<a href="#" class="list-group-item list-group-item-info" id='+em.id+'>'+em.name+'</a>'
+			});
+			$('#sflist').html(str);
+			str='';
+			$('#powlist1').html('');
+			$('#powlist2').html('');
+			$('#powlist3').html('');
+			_.map(this.powData,function(em){
+				str=' <li><label><input type="checkbox" id='+em.id+' checked='+em.v+'> '+em.pname+'</label></li>'
+				
+				$('#powlist'+parseInt(em.id/10)).append(str);				
+			});
+		}.bind(this);
+		this.loaddata();
+	};
 
 
 
