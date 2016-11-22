c2db9b3b890c6345c3065d4ec209a4f0003f9463
qiaobot <35321964@qq.com>
2016-11-22 22:25:29 +0800

    登录界面完成整合

---
+++ 后台页面/WebRoot/js/login-soft.js
@@ -0,0 +1,96 @@
+var Login = function () {
+
+
+    var handleUniform = function () {
+        if (!jQuery().uniform) {
+            return;
+        }
+        var test = $("input[type=checkbox]:not(.toggle), input[type=radio]:not(.toggle, .star)");
+        if (test.size() > 0) {
+            test.each(function () {
+                if ($(this).parents(".checker").size() == 0) {
+                    $(this).show();
+                    $(this).uniform();////鎻掍欢杩愮敤鏂规硶
+                }
+            });
+        }
+    }
+
+	var handleLogin = function() {
+		$('.login-form').validate({
+	            errorElement: 'span', //default input error message container
+	            errorClass: 'help-block', // default input error message class
+	            focusInvalid: false, // do not focus the last invalid input
+	            rules: {
+	                username: {
+	                    required: true
+	                },
+	                password: {
+	                    required: true
+	                },
+	                remember: {
+	                    required: false
+	                }
+	            },
+
+	            messages: {
+	                username: {
+	                    required: "鐢ㄦ埛鍚嶅繀濉�
+	                },
+	                password: {
+	                    required: "瀵嗙爜蹇呭～"
+	                }
+	            },
+
+	            invalidHandler: function (event, validator) { //display error alert on form submit   
+	                $('.alert-danger', $('.login-form')).show();
+	            },
+
+	            highlight: function (element) { // hightlight error inputs
+	                $(element)
+	                    .closest('.form-group').addClass('has-error'); // set error class to the control group
+	            },
+
+	            success: function (label) {
+	                label.closest('.form-group').removeClass('has-error');
+	                label.remove();
+	            },
+
+	            errorPlacement: function (error, element) {
+	                error.insertAfter(element.closest('.input-icon'));
+	            },
+
+	            submitHandler: function (form) {
+	                form.submit();
+	            }
+	        });	
+			
+	        $('.login-form input').keypress(function (e) {
+	            if (e.which == 13) {
+	                if ($('.login-form').validate().form()) {
+	                    $('.login-form').submit();
+	                }
+	                return false;
+	            }
+	        });
+	}
+    
+    return {
+        //main function to initiate the module
+        init: function () {
+        	
+            handleLogin();
+			handleUniform();
+	       	$.backstretch([
+				"image/bg/1.jpg",
+				"image/bg/2.jpg",
+				"image/bg/3.jpg"
+		        ], {
+		          fade: 1000,
+		          duration: 4000
+		    });
+        }
+
+    };
+
+}();
