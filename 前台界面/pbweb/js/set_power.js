8d5ef347b4e4b2def93027be6d5f6a1072c1dc5f
hnspoc <18349995@qq.com>
2016-11-23 04:02:36 +0800

    权限设置脚本和页面工作完成，后台请补充。

--- 前台界面/pbweb/js/set_power.js
+++ 前台界面/pbweb/js/set_power.js
@@ -2,29 +2,58 @@
 ///权限设置方法：二进制授权法--------0011 = 0001 | 0010授权       回收code = 用户权限&(~权限值)   鉴权num = code & num; (“权限”=“授权码”&“权限”)
 //权限字段为long，对应权限种类63种
 //权限名对应值    
-/*json 数据格式[sf:[{id:1,name:'系统管理员',upow:3},{id:2,name:'普通教师',pow:7}], id代表身份的唯一性，pow是身份的权限值
+/*json 客户端收数据格式
+			   [sf:[{id:1,name:'系统管理员',upow:3},{id:2,name:'普通教师',upow:7}], id代表身份的唯一性，pow是身份的权限值
                 qxm:[{qid:11,value:1,pname:'土地租赁审批'},{qid:12,value:2,pname:'实习审批'}],  qid是权限名的id 12值中的1代表它属于第一类（类别的意思）。value是权限授权码
-			   ]*/
+			   ]
+			   
+text 发服务器数据格式
+				(1,3,"系统管理员"),(2,7,"普通教师"),(3,31,"租赁业务专员"),(4,63,"实习业务专员")
+			   
+************************************************************后台开发人员请修改两处ajax的服务包名称************************************************			   
+
 var data=[{
 			sf:[{id:1,name:'系统管理员',upow:3},{id:2,name:'普通教师',upow:7},{id:3,name:'租赁业务专员',upow:31},{id:4,name:'实习业务专员',upow:63}],
 		   qxm:[{qid:11,value:1,pname:'土地租赁审批'},{qid:12,value:2,pname:'实习审批'},{qid:13,value:4,pname:'实习基地审批'},{qid:21,value:8,pname:'基地维护审批'},{qid:22,value:16,pname:'土地租赁维护'},{qid:31,value:32,pname:'租赁分析'}]
-	     }]
+	     }]*/
 
 $(function () {
 
-	
+	function closeboxEX() {
+			powman.loaddata();	
+			powman.dialog.modal('hide');		  
+	}
 	var powman=new function () {
-		this.sfData=data[0].sf
-		this.powlist=data[0].qxm
-
+		this.sfData;
+		this.powlist;
+		this.dialog;
 		
 		this.loaddata= function(){
+			var obj=this;
+///////////////////////////////////////////////////////此处修改///////////////////////////////////
+			$.ajax({                
+				type : 'POST',
+				dataType : 'json',
+				url : 'baseInfo.do',//后台修改
+				async : false,
+				cache : false,
+				error : function(request) {
+				  bootbox.alert({
+					  message: "加载失败",
+					  size: 'small'
+				  });
+				},
+				success : function(em) {
+					obj.sfData=em[0].sf;
+					obj.powlist=em[0].qxm;
+				}
+			});
+			
 			str='<a href="#" class="list-group-item active">管理身份列表</a>';
 			 _.map(this.sfData,function(em){			
 				 str=str+'<a href="#" class="list-group-item people" upow='+em.upow+' id='+em.id+'>'+em.name+'</a>';
 			});
 			$('#sflist').html(str);
-			
 			$('#powlist1').html('');
 			$('#powlist2').html('');
 			$('#powlist3').html('');
@@ -33,6 +62,79 @@ $(function () {
 				$('#powlist'+parseInt(em.qid/10)).append(str);				
 			});
 		}.bind(this);
+		
+		this.addrole= function(){
+			var uname= $('#role_text').val();
+			if(uname=='')return false;
+			var uid=new Date().getTime();
+			$('#sflist .list-group-item-info').removeClass('list-group-item-info');
+			$('#sflist').append('<a href="#" class="list-group-item list-group-item-info people" upow=0 id='+uid+'>'+uname+'</a>');
+			$('#role_text').val('');
+			$('#pow_list input[type="checkbox"]').prop('checked',false);
+			
+			return false;
+		}
+		this.delrole= function(){
+			var a=$('#sflist .list-group-item-info').attr('upow')
+			if(typeof(a) == "undefined")
+			{
+				 bootbox.alert({
+					  message: "请选择下方'身份列表'中的任一项",
+					  size: 'small'
+				  });
+				  return false;
+			}
+			 bootbox.confirm({ 
+					size: "small",
+					message: "确定删除该身份吗?", 
+					callback: function(result){
+						if(result==true)
+						{
+							$('#sflist .list-group-item-info').remove();
+							$('#pow_list input[type="checkbox"]').prop('checked',false);
+							$('#role_text').val('');
+							
+						}
+				  }
+                });	
+			
+			return false;
+		};
+		
+		this.powsave = function(){
+			var str='';
+			var i=0;
+			$('#sflist .people').each(function() {
+				if(i==0)
+					str=str+' ('+$(this).attr('id') +','+  $(this).attr('upow')+',"'  + $(this).html()+'")';
+				else
+					str=str+',('+$(this).attr('id') +','+  $(this).attr('upow')+',"'  + $(this).html()+'")';
+				i++;
+			});
+			var obj=powman;
+			///////////////////////////////////////////////////////此处修改///////////////////////////////////
+			$.ajax({                
+				type : 'POST',
+				dataType : 'text',
+				url : 'baseInfo.do',//后台修改
+				async : false,
+				cache : false,
+				error : function(request) {
+				  bootbox.alert({
+					  message: "保存失败",
+					  size: 'small'
+				  });	
+				},
+				success : function() {
+					  obj.dialog = bootbox.dialog({
+						  message: '<p class="text-center">数据提交成功，正返回中......</p>',
+						  closeButton: false
+					  });
+					  window.setTimeout(closeboxEX, 1000);						
+				}
+			});
+			return false;	
+		}
 		
 		$(document).on("click", "#sflist .people", function() {
 			$('.people').removeClass('list-group-item-info');
@@ -59,6 +161,7 @@ $(function () {
 					  message: "请选择左侧'身份列表'中的任一项",
 					  size: 'small'
 				  });
+				  return false;
 			}
 			a=parseInt(a);
 			var b=parseInt($(this).attr('pow'));
@@ -68,10 +171,10 @@ $(function () {
 				a=a&(~b);
 			$('#sflist .list-group-item-info').attr('upow',a);
 		});
+		$('#role_add').click(this.addrole);
+		$('#role_del').click(this.delrole);
+		$('#pow_update').click(this.powsave);
 		this.loaddata();
 	};
-
-
-
 
 });
