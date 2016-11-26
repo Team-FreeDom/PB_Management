71aa071cbc1cc006b4e27a8205de5f37b5bf7602
hnspoc <18349995@qq.com>
2016-11-26 14:37:32 +0800

    修复kg.js

--- 后台页面/WebRoot/js/kg.js
+++ 后台页面/WebRoot/js/kg.js
@@ -1,4 +1,15 @@
 $("li.menuItem.nav-parent").click(function(){
 	  $(this).toggleClass("opened nav-expanded");
 	});
+ $(function () {
+	 	$('#post1').attr('height',$('#calendar').attr('height'));
+	 	$(document).on("click", "#msgtable tr", function() {
+			bootbox.dialog({
+    		title: $(this).find('td').eq(1).html(),
+    		message: $(this).find('td').eq(2).html()
+			});
+			$(this).removeClass('read');
+			$(this).addClass('read');
+		})
+ });
 
