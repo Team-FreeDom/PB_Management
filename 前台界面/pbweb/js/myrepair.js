dba4c8d9c9966a36da3a8805d862e25be8c3e3f4
zaoying1027 <1747266224@qq.com>
2017-01-02 16:46:04 +0800

    页面修改，添加显示实际金额

--- 前台界面/pbweb/js/myrepair.js
+++ 前台界面/pbweb/js/myrepair.js
@@ -1,23 +1,12 @@
 // JavaScript Document
-var obj=[];
+var obj1=[];
 var obj2=[];
 
-//获取基地名称列表，在筛选中
-$.ajax({
-	url : '',
-	type : 'post',
-	dataType : 'json',			
-	success : function(data) {						         
-		for ( var i=0;i<data.length;i++) {
-				$("#Optionbase").after("<option value="+data[i].basename+">"+ data[i].applydp + "</option>");
-				}
-		}
-});	
-
-$(document).ready(function() {//点击筛选按钮并清空数据
+$(document).ready(function() {
+	
+//点击筛选按钮并清空数据
 $(".icon-filter").on("click", function () {
-	$("#baseId").val("1");
-	$("#nameId").val("");
+	$("#status").val("-1");
 	$('#hide_ul').toggle();
 });
 				
@@ -35,12 +24,7 @@ var page1=$('#myrepair').dataTable(
 					"type" : "POST"
 				},
 		"aoColumns" : [
-			{
-				"mData" : "id",					
-				"orderable" : false, // 禁用排序
-				"sDefaultContent" : "",
-				//"sWidth" : "",
-				},
+			
 			{
 				"mData" : "projectname",					
 				"orderable" : false, // 禁用排序
@@ -74,6 +58,13 @@ var page1=$('#myrepair').dataTable(
 			{
 				"mData" : "status",					
 				"orderable" : false, // 禁用排序
+				"sDefaultContent" : "",
+				//"sWidth" : "",
+				},
+			{
+				"mData" : "money",					
+				"orderable" : false, // 禁用排序
+				"visible":false,
 				"sDefaultContent" : "",
 				//"sWidth" : "",
 				},
@@ -104,7 +95,7 @@ var page1=$('#myrepair').dataTable(
 				"sDefaultContent" : "",
 				//"sWidth" : "",
 				"render":function(data,type,row){					
-						obj.push(row);							
+						obj1.push(row);							
 						if(statusid==2){
 								return data='<button type="button" class="btn btn-warning btn-xs" id="scan" value="'+(obj1.length-1)+'">查看</button>'+
                                     '<button type="button" class="btn btn-danger btn-xs" id="cancel" value="'+(obj1.length-1)+'">撤回</button>';
@@ -135,8 +126,9 @@ var page1=$('#myrepair').dataTable(
 });
 			  
 	  
-var page2=$('#myapply2').dataTable(
+var page2=$('#myrepair2').dataTable(
 	{
+	"serverSide" : true,
 	"bSort": false,
 	"bFilter": false,
 	"aLengthMenu":[2,4,6,8], //动态指定分页后每页显示的记录数。
@@ -148,12 +140,6 @@ var page2=$('#myapply2').dataTable(
 					"type" : "POST"
 				},
 		"aoColumns" : [
-			{
-				"mData" : "id",					
-				"orderable" : false, // 禁用排序
-				"sDefaultContent" : "",
-				//"sWidth" : "",
-				},
 			{
 				"mData" : "projectname",					
 				"orderable" : false, // 禁用排序
@@ -190,6 +176,14 @@ var page2=$('#myapply2').dataTable(
 				"sDefaultContent" : "",
 				//"sWidth" : "",
 				},
+			{
+				"mData" : "money",					
+				"orderable" : false, // 禁用排序
+				"visible":false,
+				"sDefaultContent" : "",
+				//"sWidth" : "",
+				},
+			
 			{
 				"mData" : "address",					
 				"orderable" : false, // 禁用排序
@@ -256,6 +250,7 @@ $(document).on("click", "#scan", function() {
 					$("#name").val(object[index].name);
 					$("#time").val(object[index].time);
 					$("#budget").val(object[index].budget);
+					$("#money").val(object[index].money);
 					$("#address").val(object[index].address);
 					$("#reason").val(object[index].reason);
 					$("#linkaddress").prop("href",object[index].linkaddress);
@@ -292,9 +287,7 @@ $(document).on("click", "#cancel", function() {
 //筛选操作
 $(document).on("click", "#finish", function() {
 		var obj2=[];
-		var baseID=$("#baseID option:selected").val();
-		var nameID=$("#nameId").val();
-		var time=$("#demo").val();
+		var status=$("#status option:selected").val();
 		page2=$('#myapply2').dataTable(
 					{
 					"bSort": false,
@@ -305,20 +298,12 @@ $(document).on("click", "#finish", function() {
 					"dom":'ftipr<"bottom"l>',
 					"ajax" : {
 								"data" : {
-								"basename" : baseID,
-								"name" : nameID,
-								"time" : time,
+								"status" : status,
 								},
 								"url" : "",
 								"type" : "POST"
 								},
 						"aoColumns" : [
-							{
-								"mData" : "id",					
-								"orderable" : false, // 禁用排序
-								"sDefaultContent" : "",
-								//"sWidth" : "",
-								},
 							{
 								"mData" : "projectname",					
 								"orderable" : false, // 禁用排序
@@ -352,6 +337,13 @@ $(document).on("click", "#finish", function() {
 							{
 								"mData" : "status",					
 								"orderable" : false, // 禁用排序
+								"sDefaultContent" : "",
+								//"sWidth" : "",
+								},
+							{
+								"mData" : "money",					
+								"orderable" : false, // 禁用排序
+								"visible":false,
 								"sDefaultContent" : "",
 								//"sWidth" : "",
 								},
