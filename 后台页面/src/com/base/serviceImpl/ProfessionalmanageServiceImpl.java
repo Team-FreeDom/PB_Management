83d623ab5d6b92edc62251e6846e338bb0ec821d
pange123 <1070170257@qq.com>
2017-06-08 01:45:37 +0800

    检查后又修复的bug

--- 后台页面/src/com/base/serviceImpl/ProfessionalmanageServiceImpl.java
+++ 后台页面/src/com/base/serviceImpl/ProfessionalmanageServiceImpl.java
@@ -12,54 +12,54 @@ import com.base.po.ProfessionalmanageList;
 import com.base.service.ProfessionalmanageService;
 
 @Service("ProfessionalmanageService")
-
 public class ProfessionalmanageServiceImpl implements ProfessionalmanageService {
 
-	@Autowired
+    @Autowired
     private ProfessionalmanageDao Professionalmanagedao;
-	
-	@Override
-	public int insertMajor(String mid,String mname,int aid) {
-		int flag = Professionalmanagedao.insertMajor(mid, mname, aid);
-		return flag;
-	}
-	
-	@Override
-	public String deleteMajor(String str){
-		String message=Professionalmanagedao.deleteMajor(str);
-		return message;
-	}
-	
-	@Override
-	public ProfessionalmanageList query_majors(int size,int pageindex,int order,String orderDir,String searchValue){
-		String columnName = "";
-		if (order == 0) {
-		    columnName = "aid";
-		} else if (order == 2) {
-		    columnName = "mid";
-		} else if (order == 3) {
-		    columnName = "mname";
-		}
-		ProfessionalmanageList list=Professionalmanagedao.query_majors(size,pageindex,
-				columnName, orderDir, searchValue);
-		return list;
-	}
 
-	@Override
-	public void updatemajor(String mid,String mname) {
-		// TODO Auto-generated method stub
-		Professionalmanagedao.updatemajor(mid,mname);
+    @Override
+    public int insertMajor(String mid, String mname, int aid) {
+	int flag = Professionalmanagedao.insertMajor(mid, mname, aid);
+	return flag;
+    }
+
+    @Override
+    public String deleteMajor(String str) {
+	String message = Professionalmanagedao.deleteMajor(str);
+	return message;
+    }
+
+    @Override
+    public ProfessionalmanageList query_majors(int size, int pageindex,
+	    int order, String orderDir, String searchValue) {
+	String columnName = "";
+	if (order == 0) {
+	    columnName = "aid";
+	} else if (order == 2) {
+	    columnName = "mid";
+	} else if (order == 3) {
+	    columnName = "mname";
 	}
-	
-	@Override
-	public int CheckmName(String name){
-		int flag = Professionalmanagedao.CheckmName(name);
-		return flag;
-	}
-	
-	@Override
-	public int CheckmName1(String name){
-		int flag = Professionalmanagedao.CheckmName1(name);
-		return flag;
-	}
+	ProfessionalmanageList list = Professionalmanagedao.query_majors(size,
+		pageindex, columnName, orderDir, searchValue);
+	return list;
+    }
+
+    @Override
+    public void updatemajor(String mid, String mname) {
+	// TODO Auto-generated method stub
+	Professionalmanagedao.updatemajor(mid, mname);
+    }
+
+    @Override
+    public int CheckmName(String name) {
+	int flag = Professionalmanagedao.CheckmName(name);
+	return flag;
+    }
+
+    @Override
+    public int CheckmName1(String name) {
+	int flag = Professionalmanagedao.CheckmName1(name);
+	return flag;
+    }
 }
